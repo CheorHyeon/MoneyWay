2372205baf00a709b33894863636a25b853bbf49
cheorhyeon <r4560798@naver.com>
2023-11-10 20:38:27 +0900

    feat : 로그인 시 시큐리티 User 객체가 아닌 DB에서 로그인 처리 되도록 변경

---
+++ src/main/java/com/wanted/moneyway/boundedContext/member/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.wanted.moneyway.boundedContext.member.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+}
