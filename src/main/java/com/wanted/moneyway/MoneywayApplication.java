3dd9585cafe5ab7d9888ca344af4319a4d5b0720
cheorhyeon <r4560798@naver.com>
2023-11-10 18:42:13 +0900

    setting : 프로젝트 세팅

---
+++ src/main/java/com/wanted/moneyway/MoneywayApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.moneyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/moneyway/MoneywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.moneyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
