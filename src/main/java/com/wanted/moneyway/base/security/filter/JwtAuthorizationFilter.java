ec9f499d9df5d97855f6479424b4020fafc2bad4
cheorhyeon <r4560798@naver.com>
2023-11-12 14:42:29 +0900

    refactor : JWT 인증 상황별 예외처리 되도록 수정

--- src/main/java/com/wanted/moneyway/base/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/wanted/moneyway/base/security/filter/JwtAuthorizationFilter.java
@@ -2,6 +2,7 @@ package com.wanted.moneyway.base.security.filter;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import javax.security.sasl.AuthenticationException;
 
@@ -15,7 +16,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import com.wanted.moneyway.base.jwt.JwtProvider;
 import com.wanted.moneyway.boundedContext.member.entity.Member;
 import com.wanted.moneyway.boundedContext.member.service.MemberService;
-import com.wanted.moneyway.standard.util.Ut;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -33,58 +33,80 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
 
+		String requestURI = request.getRequestURI();
+
+		// 회원가입 및 로그인 요청에 대해서는 필터를 건너뛰기
+		if (Pattern.matches("/api/.*/member/signup", requestURI) || Pattern.matches("/api/.*/member/login", requestURI)) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		// swagger 문서 접속에 대해서도 필터 건너뛰기
+		if (Pattern.matches("/v3/api-docs/.*", requestURI) || Pattern.matches("/swagger-ui/.*", requestURI) || requestURI.startsWith("/swagger-ui.html")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
 		String bearerToken = request.getHeader("Authorization");
 		String refreshToken = request.getHeader("RefreshToken");
 
-		// JWT의 Accss Token이 유효한 경우 사용자 정보 추출해서 바로 로그인 처리
+		if (bearerToken == null && refreshToken == null) {
+			throw new AuthenticationException("AT와 RT 둘 중 하나는 헤더에 포함시켜주세요");
+		}
+		// AT가 유효하다면 인증처리
 		if (bearerToken != null && isTokenValid(bearerToken.substring("Bearer ".length()))) {
 			forceAuthentication(getMemberFromToken(bearerToken.substring("Bearer ".length())));
+			filterChain.doFilter(request, response);
+			return;  // 다음 필터로 전달되어도 이 메서드는 종료되지 않고 계속 실행하므로 명시적 종료
 		}
-
-		// JWT AT가 유효하지 않은 경우 RT를 검사해서 RT가 아직 유효한지 확인
-		else if (refreshToken != null && isTokenValid(refreshToken)) {
+		// AT유효하지 않은데, RT가 없다면
+		if (refreshToken == null) {
+			throw new AuthenticationException("만료된 AT입니다. RT를 포함시켜주세요.");
+		}
+		// RT 유효한지 검사
+		if (isTokenValid(refreshToken)) {
 			Member member = getMemberFromToken(refreshToken);
-			// DB에 저장된 RT와 비교하여 같을 경우 강제 로그인 처리
-			if(member.getRefreshToken().equals(refreshToken)) {
-				Map<String, Object> newMemberClaims = member.toClaims();;
-				// 1일짜리 JWT AT 설정
+			// 사용자 DB에 저장된 RT와 같다면
+			if (member.getRefreshToken().equals(refreshToken)) {
+				Map<String, Object> newMemberClaims = member.toClaims();
 				String newAccessToken = jwtProvider.genToken(newMemberClaims, 60 * 60 * 24 * 1);
-				// 새로운 AT를 response 헤더에 추가하여 반환
 				response.addHeader("Authorization", "Bearer " + newAccessToken);
-				// 로그인 처리
 				forceAuthentication(member);
 			}
+			// 다르다면 변경된 RT이므로 재 로그인
+			else {
+				throw new AuthenticationException("유효하지 않은 RT입니다. 재 로그인 해주세요.");
+			}
+		} else {
+			throw new AuthenticationException("토큰이 만료되었습니다. 재 로그인 해주세요.");
 		}
+
 		filterChain.doFilter(request, response);
 	}
 
-	private Member getMemberFromToken(String token) {
+	private Member getMemberFromToken(String token) throws AuthenticationException {
 		Map<String, Object> claims = jwtProvider.getClaims(token);
-		long id = (int)claims.get("id");
-		return memberService.get(id);
+		long id = (long)claims.get("id");
+		Member member = memberService.get(id);
+		if(member == null)
+			throw new AuthenticationException("존재하지 않는 사용자입니다.");
+		return member;
 	}
 
 	private boolean isTokenValid(String token) {
 		return jwtProvider.verify(token);
 	}
 
-	// 강제로 로그인 처리하는 메소드
 	private void forceAuthentication(Member member) {
 		User user = new User(member.getUserName(), member.getPassword(), member.getGrantedAuthorities());
-
-		// 스프링 시큐리티 객체에 저장할 authentication 객체를 생성
 		UsernamePasswordAuthenticationToken authentication =
 			UsernamePasswordAuthenticationToken.authenticated(
 				user,
 				null,
 				member.getGrantedAuthorities()
 			);
-
-		// 스프링 시큐리티 내에 우리가 만든 authentication 객체를 저장할 context 생성
 		SecurityContext context = SecurityContextHolder.createEmptyContext();
-		// context에 authentication 객체를 저장
 		context.setAuthentication(authentication);
-		// 스프링 시큐리티에 context를 등록
 		SecurityContextHolder.setContext(context);
 	}
 }
