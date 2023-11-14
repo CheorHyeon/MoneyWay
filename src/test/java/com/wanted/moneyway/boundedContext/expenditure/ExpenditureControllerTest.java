7735644eed8e737fd790e4d9924648b4014433d3
cheorhyeon <r4560798@naver.com>
2023-11-14 20:26:59 +0900

    test : 지출 내역 상세 조회 테스트

--- src/test/java/com/wanted/moneyway/boundedContext/expenditure/ExpenditureControllerTest.java
+++ src/test/java/com/wanted/moneyway/boundedContext/expenditure/ExpenditureControllerTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -126,5 +127,65 @@ public class ExpenditureControllerTest {
 			.andExpect(status().is2xxSuccessful())
 			.andExpect(jsonPath("$.resultCode").value("F-1"))
 			.andExpect(jsonPath("$.msg").value("내역 작성한 사용자만 삭제 가능합니다."));
+	}
+
+	@Test
+	@DisplayName("GET /api/v1/expenditure/{id} 는 사용자가 작성한 지출 내역의 상세 내용을 반환한다.")
+	void t4() throws Exception {
+		// When
+		ResultActions resultActions = mvc
+			.perform(
+				get("/api/v1/expenditure/1")
+					.header("Authorization", "Bearer " + token) // 생성한 토큰을 헤더에 포함
+			)
+			.andDo(print());
+
+		// Then
+		resultActions
+			.andExpect(status().is2xxSuccessful())
+			.andExpect(jsonPath("$.resultCode").value("S-1"))
+			.andExpect(jsonPath("$.msg").value("내역 조회 성공"))
+			.andExpect(jsonPath("$.data.expenditureId").value(1))
+			.andExpect(jsonPath("$.data.categoryId").value(1))
+			.andExpect(jsonPath("$.data.spendingPrice").value(10000))
+			.andExpect(jsonPath("$.data.memo").value("식당 내기 짐"))
+			.andExpect(jsonPath("$.data.spendDate").value("2023-11-01"))
+			.andExpect(jsonPath("$.data.isTotal").value(true));
+	}
+
+	@Test
+	@DisplayName("GET /api/v1/expenditure/{id} 는 다른 사용자가 작성한 내역을 검사할 수 없다.")
+	void t5() throws Exception {
+		// When
+		ResultActions resultActions = mvc
+			.perform(
+				get("/api/v1/expenditure/4")
+					.header("Authorization", "Bearer " + token) // 생성한 토큰을 헤더에 포함
+			)
+			.andDo(print());
+
+		// Then
+		resultActions
+			.andExpect(status().is2xxSuccessful())
+			.andExpect(jsonPath("$.resultCode").value("F-1"))
+			.andExpect(jsonPath("$.msg").value("지출 내역 작성자가 아닙니다."));
+	}
+
+	@Test
+	@DisplayName("GET /api/v1/expenditure/{id} 는 삭제되었거나 존재하지 않는 지출내역의 정보를 조회할 수 없다.")
+	void t6() throws Exception {
+		// When
+		ResultActions resultActions = mvc
+			.perform(
+				get("/api/v1/expenditure/1052")
+					.header("Authorization", "Bearer " + token) // 생성한 토큰을 헤더에 포함
+			)
+			.andDo(print());
+
+		// Then
+		resultActions
+			.andExpect(status().is2xxSuccessful())
+			.andExpect(jsonPath("$.resultCode").value("F-1"))
+			.andExpect(jsonPath("$.msg").value("이미 삭제되었거나 존재하지 않는 내역입니다."));
 	}
 }
