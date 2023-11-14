3b3161ba664094d9ca9b856e78deeeb80cdd97ec
cheorhyeon <r4560798@naver.com>
2023-11-14 11:41:37 +0900

    test : 지출 내역 삭제 test

--- src/test/java/com/wanted/moneyway/boundedContext/expenditure/ExpenditureControllerTest.java
+++ src/test/java/com/wanted/moneyway/boundedContext/expenditure/ExpenditureControllerTest.java
@@ -6,7 +6,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.nio.charset.StandardCharsets;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -75,5 +74,57 @@ public class ExpenditureControllerTest {
 			.andExpect(jsonPath("$.data.categoryId").value(1))
 			.andExpect(jsonPath("$.data.spendingPrice").value(10_000))
 			.andExpect(jsonPath("$.data.memo").value("식당 내기 짐"));
+	}
+
+	@Test
+	@DisplayName("DELETE /api/v1/expenditure 은 지출 내역을 삭제한다.")
+	void t2() throws Exception {
+		// When
+		ResultActions resultActions = mvc
+			.perform(
+				delete("/api/v1/expenditure")
+					.header("Authorization", "Bearer " + token) // 생성한 토큰을 헤더에 포함
+					.content("""
+						{
+							  "expenditureId": "1"
+						}
+						""".stripIndent())
+					.contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+			)
+			.andDo(print());
+
+		// Then
+		resultActions
+			.andExpect(status().is2xxSuccessful())
+			.andExpect(jsonPath("$.resultCode").value("S-1"))
+			.andExpect(jsonPath("$.msg").value("삭제 성공"));
+	}
+
+	@Test
+	@DisplayName("DELETE /api/v1/expenditure 은 작성자만 지출 내역을 삭제가 가능하다")
+	void t3() throws Exception {
+		// "user3"에 해당하는 사용자 정보를 로드하고 JWT 토큰 생성
+		Member member = memberService.get("user3");
+		token = jwtProvider.genToken(member.toClaims(), 60 * 60 * 24 * 1);
+
+		// When
+		ResultActions resultActions = mvc
+			.perform(
+				delete("/api/v1/expenditure")
+					.header("Authorization", "Bearer " + token) // 생성한 토큰을 헤더에 포함
+					.content("""
+						{
+							  "expenditureId": "1"
+						}
+						""".stripIndent())
+					.contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+			)
+			.andDo(print());
+
+		// Then
+		resultActions
+			.andExpect(status().is2xxSuccessful())
+			.andExpect(jsonPath("$.resultCode").value("F-1"))
+			.andExpect(jsonPath("$.msg").value("내역 작성한 사용자만 삭제 가능합니다."));
 	}
 }
