fe3a85a62a337db5394a42f60f20687ad389f780
cheorhyeon <r4560798@naver.com>
2023-11-24 16:38:14 +0900

    refactor : 카테고리 조회 시 Redis를 활용하도록 수정

--- src/main/java/com/wanted/moneyway/base/redis/RedisService.java
+++ src/main/java/com/wanted/moneyway/base/redis/RedisService.java
@@ -1,9 +1,13 @@
 package com.wanted.moneyway.base.redis;
 
+import java.util.List;
+
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import com.wanted.moneyway.boundedContext.category.entity.Category;
+import com.wanted.moneyway.boundedContext.category.repository.CategoryRepository;
 import com.wanted.moneyway.boundedContext.member.entity.Member;
 import com.wanted.moneyway.boundedContext.member.service.MemberService;
 
@@ -15,9 +19,21 @@ public class RedisService {
 
 	private final MemberService memberService;
 
+	private final CategoryRepository categoryRepository;
+
 	@Cacheable(value = "Refresh", key = "#targetId")
 	public String getRefreshTokenByCached(long targetId) {
 		Member member = memberService.get(targetId);
 		return member.getRefreshToken();
+	}
+
+	@Cacheable(value = "CategoryList")
+	public List<Category> getAllCategoriesByCached() {
+		return categoryRepository.findAll();
+	}
+
+	@Cacheable(value = "Category", key = "#categoryId")
+	public Category getCategoryByCached(Long categoryId) {
+		return categoryRepository.findById(categoryId).orElse(null);
 	}
 }
